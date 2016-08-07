b856cc4f9e3f19c477decbd298fb6f9b8b785dbc
setzer22 <jsanchezfsms@gmail.com>
2016-08-07 12:45:06 +0200

    Fixed some problems with the implementation and updated the README file:
       - The file-checking code is no longer a macro, now functions are used.
       - The optional arity was moved from send-file to all of the send-* functions
         so the optional "options" map can be provided directly.
       - The README file now contains the new implemented methods with an example.

--- src/morse/api.clj
+++ src/morse/api.clj
@@ -30,42 +30,56 @@
          resp (http/get url {:as :json :query-params query})]
      (-> resp :body))))
 
-(defn send-file 
-  ([token chat-id file method field filename] 
-   (send-file token chat-id {} file method field filename))
-  ([token chat-id options file method field filename]
-   (let [url (str base-url token method)
-         base-form [{:part-name "chat_id" :content (str chat-id)}
-                    {:part-name field :content file :name filename}]
-         options-form (for [[key value] options]
-                        {:part-name (name key) :content value})
-         form (into base-form options-form)
-         resp (http/post url {:as :json :multipart form})]
-     (-> resp :body))))
+(defn send-file [token chat-id options file method field filename]
+  "Helper function to send various kinds of files as multipart-encoded"
+  (let [url (str base-url token method)
+        base-form [{:part-name "chat_id" :content (str chat-id)}
+                   {:part-name field :content file :name filename}]
+        options-form (for [[key value] options]
+                       {:part-name (name key) :content value})
+        form (into base-form options-form)
+        resp (http/post url {:as :json :multipart form})]
+    (-> resp :body)))
 
-(defmacro accepted-formats [file valid-extensions & body]
-  "If file is an instance of java.io.File, it checks its extension is valid.
-  Only files are checked, if the file argument is not an instance of File
-  the check will be omitted." 
-  `(if (or (not= (type ~file) java.io.File) 
-           ~@(map (fn [extension] `(.endsWith (.getName ~file) ~extension)) valid-extensions))
-     (do ~@body)
-     (throw (ex-info (str "Telegram API only supports the following formats: " 
-                          ~(string/join ", " valid-extensions) 
-                          " for this method. Other formats may be sent with send-document") 
-                     {}))))
+(defn is-file? 
+  "Is value a file?" 
+  [value] (= java.io.File (type value)))
+(defn of-type? 
+  "Does the extension of file match any of the extensions in valid-extensions?"
+  [file valid-extensions] (some #(.endsWith (.getName file) %) valid-extensions))
+(defn assert-file-type 
+  "Throws if value is a file but it's extension is not valid."
+  [value valid-extensions]
+  (when (and (is-file? value)
+             (not (of-type? valid-extensions)))
+    (throw (ex-info (str "Telegram API only supports the following formats: " 
+                         (string/join ", " valid-extensions) 
+                         " for this method. Other formats may be sent using send-document") 
+                    {}))))
 
-(defn send-photo [token chat-id image]
-  (accepted-formats image ["jpg" "jpeg" "gif" "png" "tif" "bmp"] 
-    (send-file token chat-id image "/sendPhoto" "photo" "photo.png")))
+(defn send-photo 
+  "Sends an image to the chat"
+  ([token chat-id image] (send-photo token chat-id {} image))
+  ([token chat-id options image] 
+   (assert-file-type image ["jpg" "jpeg" "gif" "png" "tif" "bmp"]) 
+   (send-file token chat-id options image "/sendPhoto" "photo" "photo.png")))
 
-(defn send-document [token chat-id document]
-  (send-file token chat-id document "/sendDocument" "document" "document"))
+(defn send-document 
+  "Sends a document to the chat"
+  ([token chat-id document] (send-document token chat-id {} document))
+  ([token chat-id options document]
+   (send-file token chat-id options document "/sendDocument" "document" "document")))
 
-(defn send-video [token chat-id video]
-  (accepted-formats video ["mp4"] 
-    (send-file token chat-id video "/sendVideo" "video" "video.mp4")))
+(defn send-video 
+  "Sends a video to the chat"
+  ([token chat-id video] (send-video token chat-id {} video))
+  ([token chat-id options video]
+   (assert-file-type video ["mp4"]) 
+   (send-file token chat-id options video "/sendVideo" "video" "video.mp4")))
 
-(defn send-audio [token chat-id audio]
-  (accepted-formats audio ["mp3"] 
-    (send-file token chat-id audio "/sendAudio" "audio" "audio.mp3")))
+(defn send-audio 
+  "Sends an audio message to the chat"
+  ([token chat-id audio] (send-audio token chat-id {} audio))
+  ([token chat-id options audio]
+   (assert-file-type audio ["mp3"]) 
+   (send-file token chat-id options audio "/sendAudio" "audio" "audio.mp3")))
