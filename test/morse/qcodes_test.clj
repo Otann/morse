4c000498de3a13b15313d26f195f9f7750034d79
Sander Kolman <Sandarr95@users.noreply.github.com>
2020-09-10 18:18:23 +0200

    more exhaustive tests for direct-reply and extract-data

--- test/morse/qcodes_test.clj
+++ test/morse/qcodes_test.clj
@@ -11,16 +11,22 @@
 
 (deftest direct-reply-handler
   (let [handler (q/direct-reply "fake-token" (h/command-fn "start" (constantly "change")))
-        start-resp (handler {:message (command-message "start")})
-        other-resp (handler {:message (command-message "stop")})
-        direct-reply-request (-> start-resp meta :direct-reply (u/capture-request))
+        handler2 (q/direct-reply "fake-token" (h/command-fn "start" (constantly {})))
+        string-resp (handler {:message (command-message "start")})
+        non-string-resp (handler2 {:message (command-message "start")})
+        nil-resp (handler {:message (command-message "stop")})
+        direct-reply-request (-> string-resp meta :direct-reply (u/capture-request))
         body    (json/decode (slurp (:body direct-reply-request)) true)]
 
     ; check direct reply does not mess with the message when it shouldn't
-    (is (= other-resp nil))
+    (is (= nil-resp nil))
     
     ; check that it returns a status 200
-    (is (= start-resp {:status 200}))
+    (is (= string-resp {:status 200}))
+    (is (= non-string-resp {:status 200}))
+
+    ; check a non string responding handler does not generate a text
+    (is (= (-> non-string-resp meta :direct-reply) nil))
     
     ; check that direct-reply is now post request
     (is (= :post (:request-method direct-reply-request)))
