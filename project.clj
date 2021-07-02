3905266162aea0d95bb2c5e53ce66b5aef6a8e9d
Mark Sto <mstoyukhin@gmail.com>
2021-07-02 18:39:13 +0300

    Update the project.clj

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
 (defproject morse "0.4.3"
-  :description "Telegram Bot API"
+  :description "Telegram Bot API client for Clojure"
 
-  :url "https://github.com/otann/morse/"
+  :url "https://github.com/otann/morse"
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/tools.macro "0.1.5"]
                  [org.clojure/core.async "0.4.474"]
+                 [org.clojure/tools.macro "0.1.5"]
                  [org.clojure/tools.logging "0.4.1"]
-                 [clj-stacktrace "0.2.8"]
                  [cheshire "5.8.1"]
-                 [clj-http "3.9.1"]]
+                 [clj-http "3.9.1"]
+                 [clj-stacktrace "0.2.8"]]
 
   :plugins [[lein-cloverage "1.0.10"]]
 
   :profiles {:uberjar {:aot :all}
              :test    {:dependencies [[clj-http-fake "1.0.3"]]
-                       :plugins      [[pjstadig/humane-test-output "0.8.2"]
-                                      [com.jakemccrary/lein-test-refresh "0.14.0"]
-                                      [com.taoensso/timbre "4.1.4"]]}}
+                       :plugins      [[com.jakemccrary/lein-test-refresh "0.14.0"]
+                                      [com.taoensso/timbre "4.1.4"]
+                                      [pjstadig/humane-test-output "0.8.2"]]}}
 
   ;; Artifact deployment info
   :scm {:name "git"
@@ -33,8 +33,15 @@
                   ["vcs" "tag"]
                   ["deploy" "clojars"]]
 
-  :pom-addition [:developers [:developer
-                              [:name "Anton Chebotaev"]
-                              [:url "http://otann.com"]
-                              [:email "anton.chebotaev@gmail.com"]
-                              [:timezone "+1"]]])
+  :pom-addition [:developers
+                 [:developer
+                  [:id "otann"]
+                  [:name "Anton Chebotaev"]
+                  [:url "http://otann.com"]
+                  [:email "anton.chebotaev@gmail.com"]
+                  [:timezone "+1"]]
+                 [:developer
+                  [:id "marksto"]
+                  [:name "Mark Sto"]
+                  [:url "https://github.com/marksto"]
+                  [:timezone "-3"]]])
